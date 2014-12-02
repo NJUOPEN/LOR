496d2fac2cf2c6b3e93d9560b926a4a48d64a5ff
TLBBrrrrr <141220098@smail.nju.edu.cn>
2014-12-02 22:30:41 +0800

    tyc地图的二维数组
    地图的二维数组

---
+++ Rtt.js
@@ -0,0 +1,35 @@
+﻿
+    //首先构建二维数组当做地面
+var ground = new Array(100);
+var count=0;
+    for (var i = 0; i <= 100; i++)
+        ground[i] = new Array(100);
+//规定：0为无法通过；1为道路；2为河流；3为可移动单位
+//整个数组归零
+    for (var i = 0; i < 100; i++)
+    {
+        for (var j = 0; j < 100; j++)
+            ground[i][j] = 0;
+    }
+ //河道
+    for (var i = 0; i < 100; i++)
+    {
+        for (var j = count; j < count+7; j++)
+            ground[i][j] = 2;
+        count++;
+    }
+//中路
+    for (var i = 99; i >= 0; i--)
+    {
+        var j = 0;
+        ground[i][j] = 1;
+        j++;
+    }
+//在屏幕上显示这个二维数组
+   
+    for (var i = 0; i < 100; i++)
+    {
+        for (var j = 0; j < 100; j++)
+            document.write(ground[i][j] );
+        document.write("<br />");
+    }
