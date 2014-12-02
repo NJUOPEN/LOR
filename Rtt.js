491534816bfb26d3c87f41ce6df6bd8be05299e1
origalin <2360436350@qq.com>
2014-12-02 23:32:57 +0800

    加了个按钮

--- Rtt.js
+++ Rtt.js
@@ -1,7 +1,8 @@
 ﻿
     //首先构建二维数组当做地面
-var ground = new Array(100);
-var count=0;
+function Grounds(){
+	var ground = new Array(100);
+	var count=0;
     for (var i = 0; i <= 100; i++)
         ground[i] = new Array(100);
 //规定：0为无法通过；1为道路；2为河流；3为可移动单位
@@ -9,20 +10,20 @@ var count=0;
     for (var i = 0; i < 100; i++)
     {
         for (var j = 0; j < 100; j++)
-            ground[i][j] = 0;
+            ground[i][j] = "_";
     }
  //河道
     for (var i = 0; i < 100; i++)
     {
         for (var j = count; j < count+7; j++)
-            ground[i][j] = 2;
+            ground[i][j] = "~";
         count++;
     }
 //中路
     for (var i = 99; i >= 0; i--)
     {
         var j = 0;
-        ground[i][j] = 1;
+        ground[i][j] = "|";
         j++;
     }
 //在屏幕上显示这个二维数组
@@ -33,3 +34,4 @@ var count=0;
             document.write(ground[i][j] );
         document.write("<br />");
     }
+	}
