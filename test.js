d2c3f3e39cdb072b9d677b2b27738c0be127f7e4
zwpwjwtz <zwpwjwtz@126.com>
2014-12-05 13:08:08 +0800

    增加事件调度、移动路径存储的模型

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 //此脚本仅供测试效果用
 
 var hero={
+	ID=1;
 	pos_x:0,
 	pos_y:0,
+	state:0;	//当前状态
+	move_path:[][][];	//缓存的移动路径，以三维数组形式存储
 	setPosition:function(x,y)
 	{
 		pos_x=x;
@@ -27,7 +30,28 @@ var hero={
         }
 		document.getElementById('header').innerHTML='('+pos_x+','+pos_y+')';
 	}
+	doEvent:function()
+	{
+		if (state=='moving')
+		{
+		
+		}
+		
+		
+	}
+}
+
+function doEvent()	//总的事件处理函数；具体处理过程交给相关对象的doEvent函数
+{
+	hero.doEvent();
+	littles.doEvent();
+	//XXX.doEvent();
+}
+
+function findSomethingByID(ID)
+{
+	if (ID==1)	return hero;
 }
 
 hero.setPosition(99,0);
-setInterval(hero.move,1000);
+setInterval(doEvent,1000);	//每隔1秒调用1次，相当于定时器
