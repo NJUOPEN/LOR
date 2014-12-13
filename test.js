5bd6cfb76c2b85ff5f9a723f7f5e46c631b1e164
origalin <2360436350@qq.com>
2014-12-13 17:21:54 +0800

    重置移动算法，改为通用类型
    语法错误待修复

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 //此脚本仅供测试效果用
 function cell_onclick(e)
 {
-	if (window.event) e=window.event;
+	if (window.event) e=window.event
 	var obj=e.srcElement;
-	if (!obj) obj=e.target;		//For Firefox
-	hero.moveTo(obj.posX,obj.posY);
+	if (!obj) obj=e.target		//For Firefox
+  moveTo(obj.posX,obj.posY,1)
 }
 document.getElementById('playArea').onclick=cell_onclick;
